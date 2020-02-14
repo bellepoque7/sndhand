261098780d7286a46bba33bb090b7927c6ece7f0
Secheon <tpcjs3002@gmail.com>
2020-02-14 19:50:22 +0900

    중고나라 크롤러 자식 클래스 Vesion 1.0

---
+++ src/JN_Crawler.py
@@ -0,0 +1,103 @@
+
+# coding: utf-8
+
+# In[16]:
+
+
+from Crawler import *
+
+class JoongnaCrawler(Crawler):
+    def __init__(self):
+        '''
+        중고나라 웹 크롤러를 생성하면 실행되는 함수
+        크롤링에 필요한 변수들을 정의한다.
+        안내 문구를 출력한다.
+        '''
+        self.driver = None
+        self.searchBox = None
+        self.data = None
+        self.title = []
+        self.price = []
+        self.date = []
+        self.goodNum = []
+        self.view = []
+        self.text = []
+        self.search = []
+        self.catg = []
+        self.loc = []
+        print("중고나라 웹 크롤러입니다.")
+    
+    def clickSearch(self):
+        '''
+        중고나라 웹 홈페이지에서 검색창으로 넘어가기 위해, 검색 버튼을 클릭하는 함수
+        '''
+        self.driver.find_element_by_xpath('//*[@id="indexHeader"]/div/div[1]/div[2]/div[3]/button/div').click()
+
+    def Scrolling(self, Num, TimeSleep):
+        '''
+        중고나라 웹 페이지를 아래로 스크롤하는 함수
+        Num : 스크롤 횟수
+        TimeSleep : 스크롤 간의 휴식 시간(초)
+        '''
+        for n in range(Num):
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(TimeSleep)
+
+    def Crawling(self, keyword, TimeSleep):
+        '''
+        게시물을 하나하나 클릭하여 내부를 크롤링하고, 이를 해당 변수에 저장한 뒤 이전 페이지로 돌아가는 함수
+        keyword : 개발자 도구에서 크롤링하고자 하는 게시물들의 class name
+        TimeSleep : 스크롤 간의 휴식 시간(초)
+        '''
+        self.driver.execute_script("window.scrollTo(0, -1 * document.body.scrollHeight);")
+        time.sleep(TimeSleep)
+        
+        for idx in range(len(self.driver.find_elements_by_class_name(keyword))):
+        
+            self.driver.find_elements_by_class_name(keyword)[idx].click()
+            time.sleep(TimeSleep)
+            
+            title_elem = JoongnaCrawler.getData(self, '//*[@id="pdtMainData"]/article[2]/div[1]/div/div[2]/div[1]/span')
+            price_elem = JoongnaCrawler.getData(self, '//*[@id="pdtMainData"]/article[2]/div[1]/div/div[2]/p/em')
+            date_elem = JoongnaCrawler.getData(self, '//*[@id="pdtMainData"]/article[2]/div[1]/div/div[2]/div[2]/dl/dd[1]')
+            goodNum_elem = JoongnaCrawler.getData(self, '//*[@id="pdtMainData"]/article[2]/div[5]/ul/li[4]/span/p')
+            view_elem = JoongnaCrawler.getData(self, '//*[@id="pdtMainData"]/article[2]/div[1]/div/div[2]/div/dl/dd[2]')
+            text_elem = JoongnaCrawler.getData(self, 'description.mt20', False)
+            search_elem = JoongnaCrawler.getData(self, 'searchKw_list', False)
+            catg_elem = JoongnaCrawler.getData(self, 'category_list', False)
+            loc_elem = JoongnaCrawler.getData(self, '//*[@id="pdtMainData"]/article[3]/dl/dd[1]/ul/li/span')
+            
+            if any([JoongnaCrawler.notCheck(self, title_elem), JoongnaCrawler.notCheck(self, price_elem), 
+                   JoongnaCrawler.notCheck(self, date_elem), JoongnaCrawler.notCheck(self, goodNum_elem),
+                   JoongnaCrawler.notCheck(self, view_elem), JoongnaCrawler.notCheck(self, tag_elem),
+                   JoongnaCrawler.notCheck(self, text_elem), JoongnaCrawler.notCheck(self, search_elem),
+                   JoongnaCrawler.notCheck(self, catg_elem), JoongnaCrawler.notCheck(self, loc_elem)]) == True:
+                
+                self.title.append(title_elem)
+                self.price.append(price_elem)
+                self.date.append(JoongnaCrawler.calcTime(self, date_elem))
+                self.goodNum.append(goodNum_elem)
+                self.view.append(view_elem)
+                self.tag.append(tag_elem)
+                self.text.append(text_elem)
+                self.search.append(search_elem)
+                self.catg.append(catg_elem)
+                self.loc.append(loc_elem)
+
+                self.driver.back()
+                time.sleep(TimeSleep)
+                
+            else:
+                self.driver.back()
+                self.driver.forward()
+                continue
+                
+    def makeDf(self):
+        '''
+        크롤링한 데이터를 데이터프레임으로 만드는 함수
+        '''
+        df = pd.DataFrame(data = {'title' : self.title, 'price' : self.price, 'date' : self.date, 
+                                  'goodNum' : self.goodNum, 'view' : self.view, 'text' : self.text, 
+                                  'search' : self.search, 'catg' : self.catg, 'loc' : self.loc})
+        return df
+
