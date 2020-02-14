ad03a21a6a4ea77b9fb31906196dc42278d6c623
Secheon <tpcjs3002@gmail.com>
2020-02-14 19:54:14 +0900

    크롤러 실행 코드 20200214

---
+++ src/JN_Crawling_ipad.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+from JoongnaCrawler import *
+
+joongna = JoongnaCrawler()
+joongna.OpenDriver("./chromedriver.exe", "https://m.joongna.com")
+time.sleep(5)
+
+joongna.ClickSearch()
+time.sleep(2)
+
+joongna.InputKeyword('//*[@id="searchStr"]', "아이패드")
+time.sleep(2)
+joongna.ExecuteSearch('//*[@id="searchHeader"]/div/div[1]/div[1]/form/button')
+time.sleep(2)
+joongna.Scrolling(Num = 10, TimeSleep = 2)
+
+joongna.Crawling(keyword = "item.goods", TimeSleep = 3)
+
+tmp = joongna.MakeDf()
+
+## 여기서 if 문을 이용해서 os.path에 JN_CrawlingData.csv가 있으면 업데이트 하는 코드 작성
+joongna.SaveDf(tmp, "JN_CrawlingData_ipad.csv", 'utf-8')
+
