6974a4bca29488903bc4980464d072ee2bca7455
Secheon <tpcjs3002@gmail.com>
2020-02-18 02:05:29 +0900

    no message

--- src/Crawler.py
+++ src/Crawler.py
@@ -31,15 +31,19 @@ class Crawler():
         driverpath : 크롬 드라이버가 위치한 경로를 입력
         webpath : 크롤링하고자 하는 사이트의 url 주소 입력
         '''
-        options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--remote-debugging-port=9222")  
-        # headless가 탐지될 떄
+        options = Options()
+        options.add_experimental_option("detach", True)
+        options.add_argument("--window-position=0,0")
+        options.add_argument("--headless")
+#         options = webdriver.ChromeOptions()
+#         options.add_argument('--headless')
+#         options.add_argument('--no-sandbox')
+#         options.add_argument("--disable-dev-shm-usage")
+#         options.add_argument("--remote-debugging-port=9222")  
+#         # headless가 탐지될 떄
         #options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
 
-        self.driver = webdriver.Chrome(driverpath, chrome_options=options, executable_path = r'/home/ubuntu/sndhand/src/chromedriver')
+        self.driver = webdriver.Chrome(driverpath, chrome_options=options)
         self.driver.get(webpath)
         
     def inputKeyword(self, xpath, keyword):
