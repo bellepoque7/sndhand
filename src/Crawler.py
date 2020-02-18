07fd411576cb218bb4e183fb4979f6f524a7a0c8
ljung <snowgot@gmail.com>
2020-02-18 23:24:58 +0900

    1st crawl

--- src/Crawler.py
+++ src/Crawler.py
@@ -34,12 +34,12 @@ class Crawler():
         '''
         options = Options()
         options.add_experimental_option("detach", True)
-        options.add_argument("--window-position=0,0")
+#        options.add_argument("--window-position=0,0")
         options.add_argument("--headless")
 #         options = webdriver.ChromeOptions()
-#         options.add_argument('--headless')
-#         options.add_argument('--no-sandbox')
-#         options.add_argument("--disable-dev-shm-usage")
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument("--disable-dev-shm-usage")
 #         options.add_argument("--remote-debugging-port=9222")  
 #         # headless가 탐지될 떄
         #options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
