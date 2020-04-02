91ae5cf530f6cf8ab6ecc476665b31859e5ab0d2
root <root@seoul-instance.asia-northeast3-a.c.secondhand-264906.internal>
2020-04-02 12:07:40 +0000

    change Testcode

--- src/TestCode_CR.py
+++ src/TestCode_CR.py
@@ -4,31 +4,33 @@
 # In[ ]:
 
 
-from JN_Crawler import *
+#from JN_Crawler import *
 from CR_Crawler import *
-from BJ_Crawler import *
+#from BJ_Crawler import *
 
-joongna = JoongnaCrawler()
-joongna.openDriver("https://m.joongna.com")
-time.sleep(5)
+#joongna = JoongnaCrawler()
+#joongna.openDriver("https://m.joongna.com")
+#time.sleep(5)
 
-joongna.clickSearch()
-time.sleep(2)
+#joongna.clickSearch()
+#time.sleep(2)
 
 #코드 변경시 키워드 "아이패드"3번등장, 28번줄의 tablet도 변경
-joongna.inputKeyword('//*[@id="searchStr"]', "아이패드")
-time.sleep(2)
-joongna.executeSearch('//*[@id="searchHeader"]/div/div[1]/div[1]/form/button')
-time.sleep(2)
-joongna.Scrolling(Num = 1, TimeSleep = 2)
+#joongna.inputKeyword('//*[@id="searchStr"]', "아이패드")
+#time.sleep(2)
+#joongna.executeSearch('//*[@id="searchHeader"]/div/div[1]/div[1]/form/button')
+#time.sleep(2)
+#joongna.Scrolling(Num = 1, TimeSleep = 2)
 
-joongna.Crawling(keyword = "item.goods", TimeSleep = 3)
+#joongna.Crawling(keyword = "item.goods", TimeSleep = 3)
 
-tmp = joongna.makeDf()
-tmp = tmp[joongna.filterData(tmp, 'tablet')]
+#tmp = joongna.makeDf()
+#tmp = tmp[joongna.filterData(tmp, 'tablet')]
 
 ## 여기서 if 문을 이용해서 os.path에 JN_CrawlingData.csv가 있으면 업데이트 하는 코드 작성
-joongna.saveDf(tmp, "JN_CrawlingData_ipad.csv", 'utf-8')
+#joongna.saveDf(tmp, "JN_CrawlingData_ipad.csv", 'utf-8')
+
+
 
 carrot = CarrotCrawler()
 carrot.openDriver('https://www.daangn.com/')
@@ -45,9 +47,13 @@ carrot.Crawling(keyword = 'flea-market-article.flat-card', TimeSleep = 3)
 tmp = carrot.makeDf()
 tmp = tmp[carrot.filterData(tmp, 'tablet')]
 
+
+
 ## 여기서 if 문을 이용해서 os.path에 JN_CrawlingData.csv가 있으면 업데이트 하는 코드 작성
 carrot.saveDf(tmp, "CR_CrawlingData_ipad.csv", 'utf-8')
 
+
+"""
 bunjang = BunjangCrawler()
 bunjang.openDriver("https://m.bunjang.co.kr/")
 time.sleep(5)
@@ -75,4 +81,4 @@ bunjang.saveDf(tmp, "BJ_CrawlingData_ipad.csv", 'utf-8')
 # 데이터 병합
 dat = bunjang.mergeDf("JN_CrawlingData_ipad.csv", "CR_CrawlingData_ipad.csv", "BJ_CrawlingData_ipad.csv")
 bunjang.saveDf(dat, "mergedData_ipad.csv", 'utf-8')
-
+"""
